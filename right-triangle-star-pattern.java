d04fbf8d79a034833d10d610648214330e661a11
akstechies <66489925+akstechies@users.noreply.github.com>
2023-04-29 16:35:48 +0530

    Create right-triangle-star-pattern.java

---
+++ right-triangle-star-pattern.java
@@ -0,0 +1,18 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++ ) {
+            // System.out.println(i);
+            for(int j = 0; j <= i; j++ ) {
+                System.out.print("* " );
+            }
+            System.out.println(" ");
+        }
+    }
+}
+
+// OutPut
+*  
+* *  
+* * *  
+* * * *  
+* * * * *  
